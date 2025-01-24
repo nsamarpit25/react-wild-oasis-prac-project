0f54d3d1ca2f7a6ade43ae483e4bea22459f0c6f
Samarpit <nsamarpit25@gmail.com>
2025-01-24 08:20:02 +0530

    bug fix: /login redirect does not work

--- src/ui/ProtectedRoute.jsx
+++ src/ui/ProtectedRoute.jsx
@@ -5,39 +5,39 @@ import styled from "styled-components";
 import { useEffect } from "react";
 
 const FullPage = styled.div`
-  height: 100vh;
-  background-color: var(--color-gray-50);
-  display: flex;
-  align-items: center;
-  justify-content: center;
+ height: 100vh;
+ background-color: var(--color-gray-50);
+ display: flex;
+ align-items: center;
+ justify-content: center;
 `;
 
 function ProtectedRoute({ children }) {
-  const navigate = useNavigate();
+ const navigate = useNavigate();
 
-  // 1. Load the authenticted user
-  const { isLoading, isAuthenticated } = useUser();
+ // 1. Load the authenticted user
+ const { isLoading, isAuthenticated } = useUser();
 
-  // 3. if there is no authenticated user redirect to login
-  useEffect(
-    function () {
-      if (!isAuthenticated && !isLoading) navigate("/login");
-    },
-    [isAuthenticated, isLoading, navigate]
+ // 3. if there is no authenticated user redirect to login
+ useEffect(
+  function () {
+   if (!isAuthenticated && !isLoading) navigate("/");
+  },
+  [isAuthenticated, isLoading, navigate]
+ );
+
+ // 2. While loading show a spinner
+ if (isLoading)
+  return (
+   <FullPage>
+    <Spinner />
+   </FullPage>
   );
 
-  // 2. While loading show a spinner
-  if (isLoading)
-    return (
-      <FullPage>
-        <Spinner />
-      </FullPage>
-    );
-
-  // 4. if there is user render the app
-  if (isAuthenticated) {
-    return children;
-  }
+ // 4. if there is user render the app
+ if (isAuthenticated) {
+  return children;
+ }
 }
 
 export default ProtectedRoute;
