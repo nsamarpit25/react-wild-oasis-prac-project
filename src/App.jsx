c01a20bf078e52c803b56177cc603c23bf05843b
Samarpit <nsamarpit25@gmail.com>
2025-01-24 08:26:34 +0530

    fix 2: problem on reload in development

--- src/App.jsx
+++ src/App.jsx
@@ -19,66 +19,104 @@ import ProtectedRoute from "./ui/ProtectedRoute";
 import { DarkModeProvider } from "./Context/DarkModeContext";
 
 const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 60 * 1000,
-    },
+ defaultOptions: {
+  queries: {
+   staleTime: 60 * 1000,
   },
+ },
 });
 
 function App() {
-  return (
-    <DarkModeProvider>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <GLobalStyles />
-        <BrowserRouter>
-          <Routes>
-            <Route
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="dashboard" element={<Daashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="bookings/:bookingId" element={<Booking />} />
-              <Route path="checkin/:bookingId" element={<Checkin />} />
-              <Route path="cabins" element={<Cabins />} />
-              <Route path="users" element={<Users />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="account" element={<Account />} />
-            </Route>
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
+ return (
+  <DarkModeProvider>
+   <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
+    <GLobalStyles />
+    <BrowserRouter>
+     <Routes>
+      <Route
+       element={
+        <ProtectedRoute>
+         <AppLayout />
+        </ProtectedRoute>
+       }
+      >
+       <Route
+        index
+        element={
+         <Navigate
+          replace
+          to="dashboard"
+         />
+        }
+       />
+       <Route
+        path="dashboard"
+        element={<Daashboard />}
+       />
+       <Route
+        path="bookings"
+        element={<Bookings />}
+       />
+       <Route
+        path="bookings/:bookingId"
+        element={<Booking />}
+       />
+       <Route
+        path="checkin/:bookingId"
+        element={<Checkin />}
+       />
+       <Route
+        path="cabins"
+        element={<Cabins />}
+       />
+       <Route
+        path="users"
+        element={<Users />}
+       />
+       <Route
+        path="settings"
+        element={<Settings />}
+       />
+       <Route
+        path="account"
+        element={<Account />}
+       />
+      </Route>
+      <Route
+       path="/login"
+       element={<Login />}
+      />
+      <Route
+       path="*"
+       element={<PageNotFound />}
+      />
+     </Routes>
+    </BrowserRouter>
 
-        <Toaster
-          position="top-center"
-          gutter={12}
-          containerStyle={{ margin: "8px" }}
-          toastOptions={{
-            success: {
-              duration: 3000,
-            },
-            error: {
-              duration: 5000,
-            },
-            style: {
-              fontSize: "16px",
-              maxWidth: "500px",
-              padding: "16px 24px",
-              backgroundColor: "var(--color-grey-0)",
-              color: "var(--color-grey-700)",
-            },
-          }}
-        />
-      </QueryClientProvider>
-    </DarkModeProvider>
-  );
+    <Toaster
+     position="top-center"
+     gutter={12}
+     containerStyle={{ margin: "8px" }}
+     toastOptions={{
+      success: {
+       duration: 3000,
+      },
+      error: {
+       duration: 5000,
+      },
+      style: {
+       fontSize: "16px",
+       maxWidth: "500px",
+       padding: "16px 24px",
+       backgroundColor: "var(--color-grey-0)",
+       color: "var(--color-grey-700)",
+      },
+     }}
+    />
+   </QueryClientProvider>
+  </DarkModeProvider>
+ );
 }
 
 export default App;
